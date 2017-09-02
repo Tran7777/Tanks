cd5b3e3751ab67eb135cd10b4f650c99e91df281
Tran7777 <mr.freeman187@gmail.com>
2017-09-03 01:54:12 +0300

    added antialasing

--- src/com/tran/display/Display.java
+++ src/com/tran/display/Display.java
@@ -2,6 +2,11 @@ package com.tran.display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
+
 //класс для создания окна (рамка и  лист) в любой момет времени существует только
 //одно окно
 public abstract class Display {
@@ -10,20 +15,26 @@ public abstract class Display {
     private static JFrame window; //создание рамки
     private static Canvas content;//добавление листа в рамку
 
+    private static BufferedImage buffer;
+    private static int[] bufferData;
+    private static Graphics bufferGraphics;
+    private static int clearColor ;
+
+private  static BufferStrategy bufferStrategy;
+//temp
+    private static float delta = 0 ;
 
 
-    public  static void create(int width, int height, String title){
+    //temp end
+    public static  void create(int width, int height, String title, int _clearColor, int numBuffers){
 
         if(created)//если окно уже созданно то не создаем новое
             return;
         window = new JFrame(title); //создание рамки
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//пр нажатии на крестик окно не только закрывается но и программаа открывается
-        content = new Canvas(){// когда создается конвас автоматически создается
+        content = new Canvas();// когда создается конвас автоматически создается
             //функция паинт и в нее передается графический обьекс с помощью канваса
 
-
-
-        };
 
         Dimension size = new Dimension(width, height);//задание размеров окна с помоощью этого класса. тк влоб нельзя
         content.setPreferredSize(size);//вставка параметров размера листа
@@ -35,9 +46,43 @@ public abstract class Display {
         window.setLocationRelativeTo(null);//поялвение окна по центру
         window.setVisible(true);
 
+        buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        bufferData = ((DataBufferInt)buffer.getRaster().getDataBuffer()).getData();
+        bufferGraphics = buffer.getGraphics();
+        clearColor = _clearColor;
+
+
+        content.createBufferStrategy(numBuffers);
+        bufferStrategy = content.getBufferStrategy();
+        created = true;
+
 
     }
 
 
+    public static void clear(){
+        Arrays.fill(bufferData, clearColor);
+
+    }
+
+    public static void render(){
+
+        bufferGraphics.setColor(new Color(0xff0000ff));
+
+        bufferGraphics.fillOval((int)(350+ Math.sin(delta)*200), 250 , 100,100);
+
+        ((Graphics2D)bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        bufferGraphics.fillOval((int)(500+ Math.sin(delta)*200), 250 , 100,100);
+
+        ((Graphics2D)bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+
+        delta+=0.02f;
+    }
+
+    public static void swapBuffers(){
+        Graphics g = bufferStrategy.getDrawGraphics();
+        g.drawImage(buffer, 0, 0 , null);
+        bufferStrategy.show();
+    }
 
 }
